b62b4e75af0d1dfb3e5108cc1bf8bb88db032515
Palash M <mpalash@gmail.com>
2018-09-25 17:42:48 +0300

    Removing progression

--- assets/scripts.js
+++ assets/scripts.js
@@ -9,58 +9,58 @@ var genres = [];
 var uniqueYears = [];
 var uniqueGenres = [];
 
-$(function() {
-  $(".all-projects .project-year-genre").each(function() {
-    years.push($(this).data("year"));
-    genres.push($(this).data("genre"));
-  });
-  uniqueYears = years
-    .unique()
-    .sort()
-    .reverse();
-  uniqueGenres = genres.unique().sort();
-
-  $.each(uniqueYears, function(i, v) {
-    $(".by-year").append(
-      "<div><span>" + v + "</span><ul data-year=" + v + "></ul></div>"
-    );
-  });
-  $.each(uniqueGenres, function(i, v) {
-    $(".by-genre").append(
-      "<div><span>" + v + "</span><ul data-genre=" + v + "></ul></div>"
-    );
-  });
-
-  $(".all-projects .project-year-genre").each(function() {
-    var y = $(this).data("year");
-    var g = $(this).data("genre");
-    var p = $(this).data("project");
-    $(this)
-      .clone()
-      .appendTo("ul[data-year=" + y + "]");
-    if (
-      $(
-        ".by-genre ul[data-genre='" +
-          g +
-          "'] .project-year-genre[data-project='" +
-          p +
-          "']"
-      ).length == 0
-    ) {
-      $(this)
-        .clone()
-        .appendTo("ul[data-genre=" + g + "]");
-    }
-  });
-
-  $(".switcher-year").on("click", function(e) {
-    e.preventDefault();
-    $(".by-genre").addClass("hide");
-    $(".by-year").removeClass("hide");
-  });
-  $(".switcher-genre").on("click", function(e) {
-    e.preventDefault();
-    $(".by-year").addClass("hide");
-    $(".by-genre").removeClass("hide");
-  });
-});
+// $(function() {
+//   $(".all-projects .project-year-genre").each(function() {
+//     years.push($(this).data("year"));
+//     genres.push($(this).data("genre"));
+//   });
+//   uniqueYears = years
+//     .unique()
+//     .sort()
+//     .reverse();
+//   uniqueGenres = genres.unique().sort();
+//
+//   $.each(uniqueYears, function(i, v) {
+//     $(".by-year").append(
+//       "<div><span>" + v + "</span><ul data-year=" + v + "></ul></div>"
+//     );
+//   });
+//   $.each(uniqueGenres, function(i, v) {
+//     $(".by-genre").append(
+//       "<div><span>" + v + "</span><ul data-genre=" + v + "></ul></div>"
+//     );
+//   });
+//
+//   $(".all-projects .project-year-genre").each(function() {
+//     var y = $(this).data("year");
+//     var g = $(this).data("genre");
+//     var p = $(this).data("project");
+//     $(this)
+//       .clone()
+//       .appendTo("ul[data-year=" + y + "]");
+//     if (
+//       $(
+//         ".by-genre ul[data-genre='" +
+//           g +
+//           "'] .project-year-genre[data-project='" +
+//           p +
+//           "']"
+//       ).length == 0
+//     ) {
+//       $(this)
+//         .clone()
+//         .appendTo("ul[data-genre=" + g + "]");
+//     }
+//   });
+//
+//   $(".switcher-year").on("click", function(e) {
+//     e.preventDefault();
+//     $(".by-genre").addClass("hide");
+//     $(".by-year").removeClass("hide");
+//   });
+//   $(".switcher-genre").on("click", function(e) {
+//     e.preventDefault();
+//     $(".by-year").addClass("hide");
+//     $(".by-genre").removeClass("hide");
+//   });
+// });
