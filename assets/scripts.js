ccb92f51c630c4151e235a0cc0ae2bdc4d6d6f03
Palash <mpalash@gmail.com>
2019-06-10 18:55:54 +0300

    Eliminating empty years

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,4 @@
-var uniqueYears = [2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2007, 2004];
+var uniqueYears = [2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009];
 var uniqueGenres = ["artwork","group-exhibition","individual-work","ongoing-project","past-project","performance","press","publication","residency","resource","solo-exhibition","talk","text","video"];
 
 $(function() {
